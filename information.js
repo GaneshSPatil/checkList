690fa13fabcab01d04a5ea848d56deaef5b51bec
ganeshpl <ganespl@INganespl.local>
2015-10-30 17:42:06 +0530

    information.js file added to store all the list (features) info and printChecklist.js added which actually prints the lists(features) that are stored in information.js

---
+++ information.js
@@ -0,0 +1,27 @@
+//all the information should be eneterd into the objects..
+var allInformation = [];
+
+
+allInformation.push({	input  : 'this is inpudht',
+						output : 'this is output',
+						status : 'Pending'
+					});
+
+allInformation.push({	input  : 'this is input',
+						output : 'this is outp,hjdskjut',
+						status : 'In development'
+					});
+
+
+allInformation.push({	input  : 'this is inpudht',
+						output : 'this is output',
+						status : 'Pending'
+					});
+
+
+allInformation.push({	input  : 'this is input',
+						output : 'this is outp,hjdskjut',
+						status : 'In development'
+					});
+
+exports.allInformation = allInformation;
