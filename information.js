80f980b3cfdffb4a4d52c387cfd0522e17568e2c
ganeshpl <ganespl@INganespl.local>
2015-10-31 10:20:57 +0530

    information.js file modified..new data appended at the end of the file while testing..also few npm nodules added..colors charm and readfilesync..printChecklist.js prints the data present in the information.js file

--- information.js
+++ information.js
@@ -1,27 +1,26 @@
 //all the information should be eneterd into the objects..
 var allInformation = [];
 
-
-allInformation.push({	input  : 'this is inpudht',
-						output : 'this is output',
-						status : 'Pending'
-					});
-
-allInformation.push({	input  : 'this is input',
-						output : 'this is outp,hjdskjut',
-						status : 'In development'
-					});
-
-
-allInformation.push({	input  : 'this is inpudht',
-						output : 'this is output',
-						status : 'Pending'
-					});
-
-
-allInformation.push({	input  : 'this is input',
-						output : 'this is outp,hjdskjut',
-						status : 'In development'
-					});
-
 exports.allInformation = allInformation;
+
+allInformation.push({	input  : 'this is inpudht',
+						output : 'this is output',
+						status : 'Pending'
+					});
+
+allInformation.push({	input  : 'this is input',
+						output : 'this is outp,hjdskjut',
+						status : 'In development'
+					});
+
+
+allInformation.push({	input  : 'this is inpudht',
+						output : 'this is output',
+						status : 'Pending'
+					});
+
+
+allInformation.push({	input  : 'this is input',
+						output : 'this is outp,hjdskjut',
+						status : 'In development'
+					});
